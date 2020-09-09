c3272d4a9d18f0d6f0521c361ea9ea16933b3ab1
domsupamoe <0jinwei0.wang@gmail.com>
2020-09-09 02:04:58 -0700

    Complete auto install for deb distro

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-BAT_VER="bat_0.15.4_amd64.deb"
-BAT_URL="https://github.com/sharkdp/bat/releases/download/v0.15.4/$BAT_VER"
+BAT_DEB="bat_0.15.4_amd64.deb"
+BAT_URL="https://github.com/sharkdp/bat/releases/download/v0.15.4/$BAT_DEB"
+
+YANK_EXE="win32yank.exe"
+YANK_ZIP="win32yank-x64.zip"
+YANK_URL="https://github.com/equalsraf/win32yank/releases/download/v0.0.4/$YANK_ZIP"
 
 # install packages
 function install_arch {
@@ -10,18 +14,33 @@ function install_arch {
 
 function install_deb {
   echo "installing in ubuntu/debian..."
+
+  sudo apt update
+  sudo apt install update-manager-core
+  sudo do-release-upgrade -d
+
+  sudo apt install software-properties-common
   sudo add-apt-repository ppa:lazygit-team/release
+
   sudo apt update
   sudo apt install \
     zsh git vim tmux build-essential \
-    neovim fzf ranger lazygit neofetch
+    neovim fzf ranger lazygit ripgrep \
+    neofetch
 
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
   cargo install exa
 
-  wget $BAT_URL -O "$HOME/$BAT_VER"
-  sudo dpkg -i "$HOME/$BAT_VER"
-  rm "$HOME/$BAT_VER"
+  if grep -q -i "microsoft" /proc/version; then
+    curl -sLo $HOME/$YANK_ZIP $YANK_URL
+    unzip -p $HOME/$YANK_ZIP $YANK_EXE > $HOME/$YANK_EXE
+    chmod +x $HOME/$YANK_EXE
+    mv $HOME/$YANK_EXE $HOME/bin && rm $HOME/$YANK_ZIP
+    # maybe need xclip for real deb distro
+  fi
+
+  wget $BAT_URL -O $HOME/$BAT_DEB
+  sudo dpkg -i $HOME/$BAT_DEB && rm $HOME/$BAT_DEB
 }
 
 # run main entry point
